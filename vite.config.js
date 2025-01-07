9700873fcf91bf711298d1dbd4b00f8a452e9d8d
Lin-Zhuo Chen <lin_zhuo_chen@163.com>
2025-01-07 13:40:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+ base:'/fds_test',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
